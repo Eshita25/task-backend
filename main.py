7c47da9792f1730dd79f093afc7bbff540fea5aa
Eshita25 <eshita04@gmail.com>
2025-10-27 12:25:10 +0530

    improved

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import json
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 app.add_middleware(
     CORSMiddleware,
